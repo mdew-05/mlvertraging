067ff903e34a056cb497994ae829297b2036ee04
mdew-05 <matthijsdewolff@gmail.com>
2026-01-07 13:59:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,6 +110,6 @@ mae = mean_absolute_error(y_test, y_pred)
 # =========================
 # Predict knop
 # =========================
-st.button("🔮 Voorspel vertraging"):
+st.button("🔮 Voorspel vertraging")
 prediction = model.predict(input_df)[0]
 st.success(f"⏱️ Verwachte vertraging: **{prediction:.1f} minuten**")
